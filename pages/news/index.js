75a4c36f54a973a71266e0bbd6b5ad865059e1b6
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-13 12:53:21 +0100

    Linking between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const News = () => {
   return (
-    <div>
+    <>
       <h1>News Page</h1>
-    </div>
+      <ul>
+        <li><Link href="/news/nextjs">NextJS is a great framework</Link></li>
+        <li><Link href="/news/something">Something Else</Link></li>
+      </ul>
+    </>
   )
 }
 
