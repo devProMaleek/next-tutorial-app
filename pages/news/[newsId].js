41b25f8f4273f486bd98f7942d84cf842130cdb4
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-13 12:37:34 +0100

    Extracting Dynamic Parameter Values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,11 +1,17 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React from 'react';
 
 const DetailsPage = () => {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+
+
   return (
     <div>
       <h1>Details Page</h1>
+      <p>{newsId}</p>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
